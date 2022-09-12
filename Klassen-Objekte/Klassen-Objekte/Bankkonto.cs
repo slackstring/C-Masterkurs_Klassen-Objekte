f22248b73bfc4f8a726e1f9b2e6a17b8e6662614
slackstring <slackstring88@gmail.com>
2022-09-12 21:30:37 +0200

    Beispiel Objekt erstellen

---
+++ Klassen-Objekte/Klassen-Objekte/Bankkonto.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Klassen_Objekte
+{
+    public class Bankkonto
+    {
+        //Eigenschaften
+        public decimal kontostand { get; set; }
+        public string besitzer    { get; set; }
+
+        //Methoden
+        public void Einzahlen(decimal betrag)
+        {
+            kontostand += betrag;
+            Console.WriteLine("Es wurden {0} EURO eingezahlt.", betrag);
+            Console.WriteLine("Neuer Kontostand: {0} EURO.", kontostand);
+        }
+
+        public void Auszahlen(decimal betrag)
+        {
+            kontostand -= betrag;
+            Console.WriteLine("Es wurden {0} EURO abgehoben.", betrag);
+            Console.WriteLine("Neuer Kontostand: {0} EURO.",kontostand);
+        }
+    }
+}
---
+++ Klassen-Objekte/Klassen-Objekte/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using Klassen_Objekte;
+
+namespace Klassenbeispiel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bankkonto kto01 = new Bankkonto();
+            kto01.kontostand = 2000;
+            kto01.besitzer = "Eileen Dapper";
+            kto01.Einzahlen(500);
+            kto01.Auszahlen(200);
+
+            Console.ReadKey();
+        }
+    }
+}
